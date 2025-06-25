f852769189373e5a2fd68c26abe0bfc450b9942f
fazategarb <legenddesign01@gmail.com>
2025-06-26 00:01:39 +0700

    feat: Update customized container terminal and add new part like header

--- src/components/Terminal.jsx
+++ src/components/Terminal.jsx
@@ -5,8 +5,12 @@ import { executeCommand } from '../data/commands';
 import { fileSystem } from '../data/fileSystem';
 
 const Terminal = () => {
+  const gradientArt = [
+    { text: 'Type "help" to see available commands.', isCommand: false },
+  ]
+
   const [output, setOutput] = useState([
-    { text: 'Welcome to my portfolio! Type "help" to see available commands.', isCommand: false }
+    ...gradientArt.map(line => ({ text: line.text, color: line.color, isCommand: false })),
   ]);
   const [currentDir, setCurrentDir] = useState('~');
   const terminalRef = useRef(null);
@@ -32,23 +36,28 @@ const Terminal = () => {
   };
 
   return (
-    <div className='h-screen flex items-center justify-center bg-gray-800 text-gray-100'>
-        <div 
-        ref={terminalRef}
-        className="bg-gray-900 text-gray-100 font-mono p-5 rounded-lg overflow-y-auto"
-        style={{
-          width: '70%', // Customize width (e.g., 600px or 50%)
-          height: '60%', // Customize height (e.g., 400px or 80vh)
-          maxWidth: '90%', // Optional: Add responsiveness
-          maxHeight: '80vh', // Optional: Prevent overflow
-        }}
-        >
-        <Output output={output} />
-        <CommandLine 
-            handleCommand={handleCommand} 
-            currentDir={currentDir}
-            />
-        </div>
+    <div className='h-[90vh] flex items-center justify-center bg-gray-800 text-gray-100'>
+      <div 
+      ref={terminalRef}
+      className="bg-gray-900 text-gray-100 font-mono p-5 rounded-lg overflow-y-auto"
+      style={{
+        width: '70%', // Customize width (e.g., 600px or 50%)
+        height: '80%', // Customize height (e.g., 400px or 80vh)
+        maxWidth: '90%', // Optional: Add responsiveness
+        maxHeight: '80vh', // Optional: Prevent overflow
+      }}
+      >
+      <div className='mb-4'>
+        <h1 className='text-2xl font-bold mb-2'>Welcome to My Portfolio Terminal</h1>
+        <p className='text-gray-400'>Explore my projects, skills, and contact information.</p>
+      </div>
+      <hr className='border-gray-500 mb-2'></hr>
+      <Output output={output} />
+      <CommandLine 
+          handleCommand={handleCommand} 
+          currentDir={currentDir}
+          />
+      </div>
     </div>
   );
 };
